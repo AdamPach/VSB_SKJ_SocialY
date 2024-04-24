61c438a09643da57265a20db6765f14713b6d510
AdamPach <adam.pach@outlook.cz>
2024-04-24 15:36:35 +0200

    #10 Add a page displaying a profile

--- SocialY/profiles/urls.py
+++ SocialY/profiles/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.show_current_profile),
+    path('<slug:username>', views.show_profile_by_username)
+]
 
--- SocialY/profiles/views.py
+++ SocialY/profiles/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
+from users.models import ApplicationUser
+
 
 # Create your views here.
+
+@login_required
+def show_current_profile(request):
+    return render(request, "profile_template.html", {"user": request.user, "is_current_user": True})
+
+
+def show_profile_by_username(request, username):
+    user = None
+
+    if request.user is not None and request.user.username == username:
+        user = request.user
+    else:
+        user = ApplicationUser.objects.filter(username=username).first()
+
+    if user is None:
+        raise Http404("Product does not exist")
+
+    return render(request, "profile_template.html", {"user": user, "is_current_user": True if username == user.username else False})
