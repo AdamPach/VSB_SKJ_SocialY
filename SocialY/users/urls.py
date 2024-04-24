0634665ffc7bf5e1aa7c90e7a30606e5041b3ddd
AdamPach <adam.pach@outlook.cz>
2024-04-24 12:29:22 +0200

    #1 Changes a paths to the auth endpoints

--- SocialY/users/urls.py
+++ SocialY/users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.users),
     path("login", views.login_user),
     path("register", views.register_user)
+    path("auth/login", views.login_user),
+    path("auth/register", views.register_user),
 ]
