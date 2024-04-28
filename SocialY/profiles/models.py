0db1bba55a65fb599d4a28fb4b8cf49568015aea
AdamPach <adam.pach@outlook.cz>
2024-04-28 19:24:17 +0200

    #10 #12 Improve the Link model

--- SocialY/profiles/models.py
+++ SocialY/profiles/models.py
@@ -6,6 +6,7 @@ from users.models import ApplicationUser
 
 class Link(models.Model):
     name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    button_text = models.CharField(max_length=100, null=False, blank=False, default="Click here")
     destination = models.URLField(unique=True, null=False, blank=False)
 
     user = models.ForeignKey(ApplicationUser, on_delete=models.CASCADE)
