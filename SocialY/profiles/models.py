7fc6b72674a05572af63843caba4b8b9a064b0c8
AdamPach <adam.pach@outlook.cz>
2024-04-28 19:06:05 +0200

    #10 #12 Add a Link db model and register it

--- SocialY/profiles/models.py
+++ SocialY/profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import ApplicationUser
+
 
 # Create your models here.
+
+class Link(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    destination = models.URLField(unique=True, null=False, blank=False)
+
+    user = models.ForeignKey(ApplicationUser, on_delete=models.CASCADE)
--- SocialY/profiles/views.py
+++ SocialY/profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import Http404, HttpResponse
+from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from users.models import ApplicationUser
 
