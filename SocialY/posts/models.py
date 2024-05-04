7416663d13c63f599aa23d89155cd00d20f7c911
AdamPach <adam.pach@outlook.cz>
2024-05-04 12:33:11 +0200

    #14 Add comment model

--- SocialY/posts/models.py
+++ SocialY/posts/models.py
@@ -12,3 +12,15 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.author.username}: {self.created_on}"
+
+
+class Comment(models.Model):
+    comment_content = models.TextField(max_length=1000, blank=False, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    author = models.ForeignKey(ApplicationUser, on_delete=models.CASCADE)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author.username}: {self.created_on}"
