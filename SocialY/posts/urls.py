87b687a2c39a3b3e2a0a6929fdc92659ef58a81a
AdamPach <adam.pach@outlook.cz>
2024-06-15 21:02:07 +0200

    Fix linting issues

--- SocialY/posts/urls.py
+++ SocialY/posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.create_post),
     path('list', views.list_posts),
-    path('list/following',views.list_followed_posts),
+    path('list/following', views.list_followed_posts),
     path('<int:id_post>', views.get_post_detail),
     path('<int:id_post>/like', views.like_post),
     path('<int:id_post>/comment', views.add_comment),
